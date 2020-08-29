ae2998a8edc6ab1d3f2e445d29cad5870f94831c
Luis Felipe Carmona Cuellar <luiscarmona@qdata.io>
2020-08-28 21:09:43 -0500

    add register modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
-function myFunction() {
+function displayMenu() {
     var element = document.getElementById("menu");
     element.classList.toggle("menu-mobile");
+}
+
+function openMod() {
+    var open = document.querySelector(".mod-domicilio");
+    var gray = document.querySelector(".total");
+    open.style.display = "flex";
+    gray.style.background = "#CFCFCF";
+    gray.style.opacity = "0.5";
+
+}
+
+function closeMod() {
+    var exit = document.querySelector(".mod-domicilio");
+    var gray = document.querySelector(".total");
+    exit.style.display = "none";
+    gray.style.background = "";
+    gray.style.opacity = "1";
+}
+
+function openModReg() {
+    var open = document.querySelector(".logIn");
+    var gray = document.querySelector(".total");
+    open.style.display = "flex";
+    gray.style.background = "#CFCFCF";
+    gray.style.opacity = "0.5";
+
+}
+
+function closeModReg() {
+    var exit = document.querySelector(".logIn");
+    var gray = document.querySelector(".total");
+    exit.style.display = "none";
+    gray.style.background = "";
+    gray.style.opacity = "1";
 }
